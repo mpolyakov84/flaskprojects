f4d4ea9686f144c85be417791a1bb9d34c84e5fd
mpolyakov84 <mpolyakov7@gmail.com>
2024-09-18 16:20:20 +0100

    Update app.py from phone

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask import render_template
 def index():  # put application's code here
     return render_template('index.html')
 #test commit from pycharm
+# I am changing whether it is possible to code here
 
 if __name__ == '__main__':
     app.run(debug=True)
