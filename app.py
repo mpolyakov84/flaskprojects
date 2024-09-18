513ad21641de156cd2431f9ab66d1785fa1ab3c9
mpolyakov84 <mpolyakov7@gmail.com>
2024-09-18 16:14:45 +0100

    Changes in app

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import render_template
 @app.route('/')
 def index():  # put application's code here
     return render_template('index.html')
-
+#test commit from pycharm
 
 if __name__ == '__main__':
     app.run(debug=True)
